62007b6cf2408fd70a85bb8486b6f76822eb7ab7
Juan Luis Martel <usblsb@mac.com>
2025-09-01 14:03:03 +0100

    Fin de la Fase 3: Actualización del Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,13 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)',
+  '/sign-out(.*)',
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) await auth.protect();
+});
 
 export const config = {
   matcher: [
